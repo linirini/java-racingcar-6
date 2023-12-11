4f466345bb78067839d09f683e62894b4ef13c3b
linirini <2001yerin@naver.com>
2023-12-12 00:19:46 +0900

    feat : implement getting try number input

--- src/main/java/racingcar/view/InputEnum.java
+++ src/main/java/racingcar/view/InputEnum.java
@@ -1,7 +1,7 @@
 package racingcar.view;
 
 public enum InputEnum {
-    CAR_NAME_INPUT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    CAR_NAMES_INPUT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     TRY_NUMBER_INPUT("시도할 횟수는 몇회인가요?");
     private String message;
 
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,11 +1,18 @@
 package racingcar.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static racingcar.view.InputEnum.CAR_NAMES_INPUT;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
-    public String inputTryNumber(){
-        return null;
+    public List<String> inputTryNumber(){
+        System.out.println(CAR_NAMES_INPUT.getMessage());
+        String input = readLine();
+        return Arrays.stream(input.split(",")).collect(Collectors.toList());
     }
 
     public List<String> inputCars(){
