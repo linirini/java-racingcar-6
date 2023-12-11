40cbc820aa76a9cff4b7edb951340121eaf0f5f4
linirini <2001yerin@naver.com>
2023-12-12 03:12:06 +0900

    test : test for car not moving forward

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,7 +3,6 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,11 +10,20 @@ public class CarTest {
 
     @DisplayName("숫자가 4 이상이면 전진한다.")
     @ParameterizedTest
-    @ValueSource(ints={4,5,6,7,8,9})
-    void 자동차_전진(int moveCount){
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void 자동차_전진(int moveCount) {
         Car car = new Car("자동차");
         car.move(moveCount);
         assertThat(car.getForwardCount()).isEqualTo(1);
+    }
+
+    @DisplayName("숫자가 4 미만이면 전진하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void 자동차_멈춤(int moveCount) {
+        Car car = new Car("자동차");
+        car.move(moveCount);
+        assertThat(car.getForwardCount()).isEqualTo(0);
     }
 
 }
