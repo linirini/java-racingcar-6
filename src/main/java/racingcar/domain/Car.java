53002f78eec41baae0bf72c314fac090b9c2eb83
linirini <2001yerin@naver.com>
2023-12-12 00:38:52 +0900

    feat : implement validating car name length

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,27 @@
 package racingcar.domain;
 
+import static racingcar.utils.ExceptionEnum.INVALID_NAME_LENGTH;
+
 public class Car {
 
+    private static final int MAX_LENGTH = 5;
     private static final String MOVE = "-";
     private final String name;
     private int movement;
 
     public Car(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(String name) {
+        throwIfInvalidNameLength(name);
+    }
+
+    private void throwIfInvalidNameLength(String name) {
+        if(name.length()>MAX_LENGTH){
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH.getMessage());
+        }
     }
 
     public int move(){
--- src/main/java/racingcar/utils/ExceptionEnum.java
+++ src/main/java/racingcar/utils/ExceptionEnum.java
@@ -1,7 +1,7 @@
 package racingcar.utils;
 
 public enum ExceptionEnum {
-    ;
+    INVALID_NAME_LENGTH("자동차 이름은 5자를 초과할 수 없습니다.");
     private static final String ERROR = "[ERROR] ";
     private String message;
 
