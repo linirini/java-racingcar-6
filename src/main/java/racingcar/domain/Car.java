6bb38e42ac1c89d3962a966e05c841a71ba29bfc
linirini <2001yerin@naver.com>
2023-12-12 01:10:36 +0900

    refactor : remove getting movement string to car and fix output view code according to the change

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -6,6 +6,7 @@ public class Car {
 
     private static final int MAX_LENGTH = 5;
     private static final int MININUM_MOVE_COUNT = 4;
+    private static final String MOVE = "-";
     private final RandomNumberGenerator numberGenerator = new RandomNumberGenerator();
     private final String name;
     private int forwardCount;
@@ -22,21 +23,29 @@ public class Car {
     }
 
     private void throwIfInvalidNameLength(String name) {
-        if(name.length()>MAX_LENGTH){
+        if (name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException(INVALID_NAME_LENGTH.getMessage());
         }
     }
 
-    public void move(){
+    public void move() {
         this.moveCount = numberGenerator.createRandomNumber();
     }
 
-    private boolean isMovingForward(){
+    private boolean isMovingForward() {
         return false;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getMovement() {
+        String move = "";
+        for (int i = 0; i < this.moveCount; i++) {
+            move = move + MOVE;
+        }
+        return move;
     }
 
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,34 +5,25 @@ import static racingcar.view.OutputEnum.RESULT_OUTPUT;
 import static racingcar.view.OutputEnum.WINNERS_OUTPUT;
 
 import racingcar.domain.Car;
-import racingcar.domain.Cars;
 import racingcar.domain.Winners;
 
 public class OutputView {
 
-    private static final String MOVE = "-";
 
-    public void printResult(){
+    public void printResult() {
         System.out.println(RESULT_OUTPUT.getMessage());
     }
 
-    public void printCarMovement(String name, int moveCount){
-        System.out.println(MOVEMENT_OUTPUT.getMovementOutput(name, getMovement(moveCount)));
+    public void printCarMovement(Car car) {
+        System.out.println(MOVEMENT_OUTPUT.getMovementOutput(car.getName(), car.getMovement()));
     }
 
-    private String getMovement(int moveCount){
-        String move = "";
-        for (int i = 0; i < moveCount; i++) {
-            move = move+MOVE;
-        }
-        return move;
-    }
 
-    public void printWinners(Winners winners){
+    public void printWinners(Winners winners) {
         System.out.println(WINNERS_OUTPUT.getWinnersOutput(winners.getWinners()));
     }
 
-    public void printLineBreak(){
+    public void printLineBreak() {
         System.out.println();
     }
 
